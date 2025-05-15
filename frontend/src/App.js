9a726890bb237509ab3492a2adc72d4a504464e5
haha096 <lucky-96@naver.com>
2025-05-15 14:53:14 +0900

    merge3

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,16 +3,6 @@ import { useEffect, useState, useRef } from "react";
 import AppRouter from "./AppRouter";
 import {BrowserRouter as Router, Routes, Route, Link} from "react-router-dom";
 
-import Login from "./Login/Login";
-import Signup from "./Login/Signup";
-import Main from "./Main";
-import Header from "./components/Header";
-import Footer from "./components/Footer";
-import DataAnalysis from "./page/DataAnalysis";
-import Monitoring_indoor from "./page/Monitoring/Monitoring_indoor";
-import Monitoring_outdoor from "./page/Monitoring/Monitoring_outdoor";
-import Device_Control from "./page/Device_Control";
-
 function App() {
     const socketRef = useRef(null);
 
@@ -71,21 +61,6 @@ function App() {
             socket={socketRef.current}
         />
 
-        <Router>
-            <Header />
-
-            <Routes>
-                <Route path="/" element={<Main message={message} />} />
-                <Route path="/login" element={<Login handleLogin={handleLogin} />} />
-                <Route path="/signup" element={<Signup />} />
-                <Route path="/dataanalysis" element={<DataAnalysis />} />
-                <Route path="/monitoring_indoor" element={<Monitoring_indoor />} />
-                <Route path="/monitoring_outdoor" element={<Monitoring_outdoor />} />
-                <Route path="/devicecontrol" element={<Device_Control />} />
-            </Routes>
-
-            <Footer />
-        </Router>
     </div>
     );
 }
