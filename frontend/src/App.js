25b0d916a3f2c9672cd9084c5f1d88f8889c2a51
haha096 <lucky-96@naver.com>
2025-05-28 16:50:50 +0900

    merge_v1

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,10 @@
 import './App.css';
-import { useEffect, useState, useRef } from "react";
+import { useEffect, useState } from "react";
 import AppRouter from "./AppRouter";
 import {BrowserRouter as Router, Routes, Route, Link} from "react-router-dom";
 
 function App() {
-    const socketRef = useRef(null);
+    const [socket, setSocket] = useState(null);
 
     const [message, setMessage] = useState("");
     const [isLoggedIn, setIsLoggedIn] = useState(false);
@@ -28,32 +28,39 @@ function App() {
     };
 
    useEffect(() => {
-     // WebSocket 연결
-     const socket = new WebSocket('ws://localhost:8080/ws/sensor');  // WebSocket 서버 주소
-     socketRef.current = socket;
+       // WebSocket 연결
+       const ws = new WebSocket('ws://localhost:8080/ws/sensor');  // WebSocket 서버 주소
 
-     // 서버로부터 메시지를 받았을 때
-     socket.onmessage = (event) => {
-     console.log("WebSocket 메시지 수신:", event.data);  // 수신한 데이터 로그
-       const data = JSON.parse(event.data);  // 메시지가 JSON 형식이라면
-       setMessage(data);  // 받은 데이터를 상태에 업데이트
-     };
+       // 서버로부터 메시지를 받았을 때
+       ws.onmessage = (event) => {
+           //console.log("WebSocket 메시지 수신:", event.data);  // 수신한 데이터 로그
+           try {
+               const data = JSON.parse(event.data);  // 메시지가 JSON 형식이라면
+               setMessage(data);
+           } catch (e) {
+               console.error("❌ JSON 파싱 실패:", e, "📨 메시지 내용:", event.data);
+               setMessage("서버에서 잘못된 형식의 데이터를 받았습니다.");
+           }
+       };
 
      // WebSocket 연결 종료 시
-     socket.onclose = () => {
+     ws.onclose = () => {
        console.log("WebSocket 연결 종료");
      };
 
      // 에러 처리
-     socket.onerror = (error) => {
+     ws.onerror = (error) => {
        console.error("WebSocket 오류 발생:", error);
        setMessage("서버 연결 실패 😢");
      };
 
-     // 컴포넌트가 언마운트 될 때 WebSocket 연결 종료
-     return () => {
-       socket.close();
-     };
+       // 컴포넌트가 언마운트 될 때 WebSocket 연결 종료
+       return () => {
+           //열린상태에서 닫기
+           if (ws.readyState === WebSocket.OPEN) {
+               ws.close();
+           }
+       };
    }, []);  // 빈 배열을 넣어서 컴포넌트가 마운트될 때만 실행
 
     //로그인 세션
@@ -84,7 +91,7 @@ function App() {
             userNickname={userNickname}
             handleLogin={handleLogin}
             handleLogout={handleLogout}
-            socket={socketRef.current}
+            socket={socket}
         />
 
     </div>
