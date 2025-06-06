d1f97747a102e2ffa4397ae30f4a49b91bdf9030
arti369 <98258020+arti369@users.noreply.github.com>
2025-06-06 16:07:28 +0900

    Delete backend directory

--- backend/src/main/java/Not_Found/util/MyUtil.java
+++
@@ -1,8 +0,0 @@
-package Not_Found.util;
-
-public class MyUtil {
-    public final static String BLUE = "\033[94m";
-    public final static String END = "\033[0m";
-
-    public final static String BOLD = "\033[1m";
-}
